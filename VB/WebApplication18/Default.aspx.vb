f32a1cdd276e0e7d65acf32f21147868917626a2
DevExpressExampleBot <fakeEmail>
2020-04-16 01:11:39 +0300

    Source auto update [skip ci]

--- VB/WebApplication18/Default.aspx.vb
+++ VB/WebApplication18/Default.aspx.vb
@@ -8,18 +8,18 @@ Imports System.Web.UI
 Imports System.Web.UI.WebControls
 
 Namespace WebApplication18
-    Partial Public Class [Default]
-        Inherits System.Web.UI.Page
+	Partial Public Class [Default]
+		Inherits System.Web.UI.Page
 
-        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-            Dim newDashboardStorage As New DashboardFileStorage("~/App_Data/Dashboards")
-            ASPxDashboard1.SetDashboardStorage(newDashboardStorage)
-        End Sub
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+			Dim newDashboardStorage As New DashboardFileStorage("~/App_Data/Dashboards")
+			ASPxDashboard1.SetDashboardStorage(newDashboardStorage)
+		End Sub
 
-        Protected Sub ASPxDashboard1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DevExpress.DashboardWeb.ConfigureDataConnectionWebEventArgs)
-            If e.DataSourceName = "SQL Data Source 1" Then
-                e.ConnectionParameters = New XmlFileConnectionParameters() With {.FileName = Server.MapPath("/App_Data/Departments.xml")}
-            End If
-        End Sub
-    End Class
+		Protected Sub ASPxDashboard1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DevExpress.DashboardWeb.ConfigureDataConnectionWebEventArgs)
+			If e.DataSourceName = "SQL Data Source 1" Then
+				e.ConnectionParameters = New XmlFileConnectionParameters() With {.FileName = Server.MapPath("/App_Data/Departments.xml")}
+			End If
+		End Sub
+	End Class
 End Namespace
--- VB/WebApplication18/Global.asax.vb
+++ VB/WebApplication18/Global.asax.vb
@@ -12,35 +12,35 @@ Imports System.Web.Security
 Imports System.Web.SessionState
 
 Namespace WebApplication18
-    Public Class [Global]
-        Inherits System.Web.HttpApplication
+	Public Class [Global]
+		Inherits System.Web.HttpApplication
 
-        Protected Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+		Protected Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
 
-        End Sub
+		End Sub
 
-        Protected Sub Session_Start(ByVal sender As Object, ByVal e As EventArgs)
+		Protected Sub Session_Start(ByVal sender As Object, ByVal e As EventArgs)
 
-        End Sub
+		End Sub
 
-        Protected Sub Application_BeginRequest(ByVal sender As Object, ByVal e As EventArgs)
+		Protected Sub Application_BeginRequest(ByVal sender As Object, ByVal e As EventArgs)
 
-        End Sub
+		End Sub
 
-        Protected Sub Application_AuthenticateRequest(ByVal sender As Object, ByVal e As EventArgs)
+		Protected Sub Application_AuthenticateRequest(ByVal sender As Object, ByVal e As EventArgs)
 
-        End Sub
+		End Sub
 
-        Protected Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
+		Protected Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
 
-        End Sub
+		End Sub
 
-        Protected Sub Session_End(ByVal sender As Object, ByVal e As EventArgs)
+		Protected Sub Session_End(ByVal sender As Object, ByVal e As EventArgs)
 
-        End Sub
+		End Sub
 
-        Protected Sub Application_End(ByVal sender As Object, ByVal e As EventArgs)
+		Protected Sub Application_End(ByVal sender As Object, ByVal e As EventArgs)
 
-        End Sub
-    End Class
+		End Sub
+	End Class
 End Namespace
